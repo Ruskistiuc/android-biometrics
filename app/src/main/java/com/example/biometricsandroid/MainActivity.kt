45323f76190235ffa5a83d7efb4df87322b983f5
Ruslan Padnevych <r_padnevych@hotmail.com>
2021-01-16 18:11:48 +0000

    Added Android Biometric Library

--- app/src/main/java/com/example/biometricsandroid/MainActivity.kt
+++ app/src/main/java/com/example/biometricsandroid/MainActivity.kt
@@ -3,6 +3,11 @@ package com.example.biometricsandroid
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * FROM:
+ * 1) https://developer.android.com/codelabs/biometric-login#0
+ * 2) https://github.com/googlecodelabs/biometric-login
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
